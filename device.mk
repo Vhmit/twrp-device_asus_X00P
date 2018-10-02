b3c71a28b626cd9a4e8f2c3f324476f7098adb4c
Ethan Chen <intervigil@gmail.com>
2018-10-02 13:51:03 +0200

    X00P: Use Soong namespace to avoid device build conflicts
    Change-Id: Id04f464102264b6f2e8d49b39389c7e346613b39

---
+++ device.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/asus/X00P
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
